89cb1f0fd6001151bdd116b2eb1b142c14f8b23b
b0463 <hkimpr40@gmail.com>
2022-05-02 03:52:37 -0300

    bootload hello world mtd1

--- src/boot.asm
+++ src/boot.asm
@@ -3,12 +3,6 @@
 
 call HelloWorld
 call JumpLine
-call PointerString
-call PrintString
-call PointerBuffer
-call ReadString
-call PointerStringBuffer
-call PrintString
 
 HelloWorld:
     mov ah, 0eh
@@ -42,44 +36,5 @@ JumpLine:
     int 10h
     ret
 
-PointerString:
-    mov si, hello
-    ret
-
-PointerBuffer:
-    mov di, buffer
-    ret
-
-PointerStringBuffer:
-    mov si, buffer
-    ret
-
-PrintString:
-    mov ah, 0eh
-    mov al, [si]
-    print:
-        int 10h
-        inc si
-        mov al, [si]
-        cmp al, 0
-        jne print
-    ret
-
-ReadString:
-    mov ah, 00h
-    int 16h
-    mov ah, 0eh
-    int 10h
-    mov [di], al
-    inc di
-    cmp al, 0dh
-    jne ReadString
-    mov ah, 1eh
-    mov al, 0ah
-    int 10h
-    ret
-
-hello db "Hello World!",13,10,0
-buffer times 20 db 0
 times 512 - ($$-$) db 0
 dw 0xAA55
