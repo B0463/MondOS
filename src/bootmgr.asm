7f988cf67c61ffbdd7b0b34522d408db6f32a177
b0463 <hkimpr40@gmail.com>
2022-05-02 23:52:29 -0300

    kernel sys menu

--- src/bootmgr.asm
+++ src/bootmgr.asm
@@ -1,5 +1,21 @@
 [BITS 16]
 [ORG 0x7C00]
 
+call LoadSystem ;carrega o kernel
+jmp 0800h:0000h ;passa o controle para o kernel
+
+LoadSystem:
+    mov ah, 02h ;carregar
+    mov al, 1 ;setores 1
+    mov ch, 0 ;trila 0
+    mov cl, 2 ;setor 2
+    mov dh, 0 ;cabesote 0
+    mov dl, 80h ;primeiro disco
+    mov bx, 0800h ;local 1
+    mov es, bx ;ex = local 1
+    mov bx, 0000h ;local 2
+    int 13h ;executa
+    ret ;retorna
+
 times 510 - ($-$$) db 0
 dw 0xAA55
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,2 +1,82 @@
 [BITS 16]
+[ORG 0000h]
 
+jmp OSMain
+
+BackWidth db 0
+BackHeight db 0
+Pagination db 0
+Welcome db "Bem-Vindo ao MondOS!",0
+
+OSMain:
+    call ConfigSegment
+    call ConfigStack
+    call TEXT.SetVideoMode
+    call BackColor
+    jmp ShowString
+
+ShowString:
+    mov dh, 3
+    mov dl, 3
+    call MoveCursor
+    mov si, Welcome
+    call PrintString
+    mov ah, 00
+    int 16h
+    jmp END
+
+ConfigSegment:
+    mov ax, es
+    mov ds, ax
+    ret
+
+ConfigStack:
+    mov ax, 7D00h
+    mov ss, ax
+    mov sp, 03FFh
+    ret
+
+TEXT.SetVideoMode:
+    mov ah, 00h
+    mov al, 03h
+    int 10h
+    mov BYTE[BackWidth], 80
+    mov BYTE[BackHeight], 20
+    ret
+
+BackColor:
+    mov ah, 06h
+    mov al, 0
+    mov bh, 1Fh
+    mov ch, 0
+    mov cl, 0
+    mov dh, 6
+    mov dl, 80
+    int 10h
+    ret
+
+PrintString:
+    mov ah, 09h
+    mov bh, [Pagination]
+    mov bl, 2Fh
+    mov cx, 1
+    mov al, [si]
+    print:
+        int 10h
+        inc si
+        call MoveCursor
+        mov ah, 09h
+        mov al, [si]
+        cmp al, 0
+        jne print
+        ret
+
+MoveCursor:
+    mov ah, 02h
+    mov bh, [Pagination]
+    inc dl
+    int 10h
+    ret
+
+END:
+    int 19h
