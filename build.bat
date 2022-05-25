98eb18936c28158b058f779789531c6aa5edef81
b0463 <hkimpr40@gmail.com>
2022-05-25 03:07:53 -0300

    refactor: restart project using cpp

--- build.bat
+++ build.bat
@@ -2,9 +2,4 @@
 cls
 echo compilando: bootmgr
 nasm -f bin src/bootmgr.asm -o bin/bootmgr
-echo compilando: kernel
-nasm -f bin src/kernel.asm -o bin/kernel.bin
-echo compilando: window
-nasm -f bin src/window.asm -o bin/window.bin
-echo.
 pause
